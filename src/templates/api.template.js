6df65a3b43609fd45063fae93f63fb0262077fa7
michaeladze <michael.kutateladze@yandex.ru>
2020-12-05 20:43:37 +0300

    Release 1.5.0

--- src/templates/api.template.js
+++ src/templates/api.template.js
@@ -2,9 +2,9 @@ const apiTemplate = (name, answers) => {
   return `
 /** ${ answers.description } */
 app.${answers.method.toLowerCase()}('/${answers.actionName}', (req, res) => {
-  fs.readFile('./mocks/${name}/${answers.actionName}.json', 'utf-8', (err, data) => {
+  fs.readFile(__dirname + './mocks/${name}/${answers.actionName}.json', 'utf-8', (err, data) => {
     if (err) return res.error();
-    return res.json(data);
+    return res.json(JSON.parse(data));
   })
 })
 `;
