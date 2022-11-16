555c401415c830e9df67268af840de8ef5225a4b
Michal Kutateladze <michaeladze@returns.co>
2022-11-16 13:25:10 +0100

    m

--- src/runLinter.ts
+++ src/runLinter.ts
@@ -1,5 +1,8 @@
 import { exec } from 'child_process';
 
+import { logger } from '../templater/utils/logger';
+
 export function runLinter(path: string) {
+  logger.info(`Running linter for ${path}`);
   exec(`eslint ${path} --fix`);
 }
